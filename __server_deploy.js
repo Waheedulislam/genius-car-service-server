17f32fb6a59d50e7250888f452e54ee72f4f24ee
Whaeedil <baoppyhossen1234@gmail.com>
2022-04-30 18:57:47 +0600

    'added-hero-end-point'

---
+++ __server_deploy.js
@@ -0,0 +1,29 @@
+/**
+ * -------------------
+ * One time for you computer
+ * -------------------
+ * 1. create a heroku account
+ * 2. verify email
+ * 3. install heroku cli
+ * 4. heroku login
+ * -------------------
+ * For each projects on time
+ * -------------------
+ * 1. heroku create
+ * 2. make sure you: git add . git commit. git push
+ * 3. git push heroku main
+ * 4. go to heroku dashboard > Current  project>settings>Reveal Config vars
+ * 5. copy paste config vars from your .env file
+ * 6. Make sure you have whitelisted all ip address to access mongodb
+ * ------------------
+ * Update server with new changes
+ * ------------------
+ * 1. Make changes
+ * 2. make sure you: git add . git commit. git push
+ * 3. git push heroku main
+ * 4
+ * ------------------
+ * Connect server with client 
+ * ------------------
+ * 
+*/
